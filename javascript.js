f93de175077ab715be29bb38a5a573edb9e6a64d
Derek Praegitzer <dpraegitzer@gmail.com>
2022-07-27 16:36:01 -0700

    CSS updates - Added buttons & text

--- javascript.js
+++ javascript.js
@@ -1,14 +1,4 @@
 function getComputerChoice() {
-	let compChoice = (Math.random() * 3);
-	
-	if (computerSelection == 1)
-		return "rock";
-	else if (computerSelection == 2)
-		return "paper";
-	else if (computerSelection == 3)
-		return "scissors";
-}
-function getComputerChoice() {
         let computerChoice = (Math.round(Math.random() * 3));
 
         console.log("computerChoice = " + computerChoice);
@@ -57,43 +47,10 @@ function singleRound(playerSelection, computerSelection) {
         }
 }
 
-let choice = "rock";
+choice = prompt(choice);
 
 let result = singleRound(choice);
 
 //console.log("result = " + result);
 alert("choice = " + choice + "\nresult = " + result);
-function singleRound(playerSelection, computerSelection) {
 
-	computerSelection == getComputerChoice();
-
-	playerSelection == playerSelection.toLowerCase();
-
-	if (playerSelection == "rock") {
-		if (computerSelection == "rock") {
-			return "Both players chose rock, it's a draw!";
-		} else if (computerSelection == "paper") {
-			return "You lose, the computer chose rock!";
-		} else if (computerSelection == "scissors") {
-			return "You win, the computer chose scissors!";
-		}
-	} else if (playerSelection == "paper") {
-		if (computerSelection == "paper") {
-                        return "Both players chose paper, it's a draw!";
-                } else if (computerSelection == "scissors") {
-                        return "You lose, the computer chose scissors!";
-                } else if (computerSelection == "rock") {
-			return "You win, the computer chose rock!";
-                }
-	} else if (playerSelection == "scissors") {
-		if (computerSelection == "scissors") {
-                        return "Both players chose scissors, it's a draw!";
-                } else if (computerSelection == "rock") {
-                        return "You lose, the computer chose rock!";
-                } else if (computerSelection == "scissors") {
-                        return "You win, the computer chose paper!";
-                }
-	} else {
-		return "Invalid entry, please choose rock, paper, or scissors.";
-	}
-}
