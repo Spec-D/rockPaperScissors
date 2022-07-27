34b13462d41836aa085e3cf92f3862fc609668b5
Spec-D <dpraegitzer@gmail.com>
2022-07-27 01:49:26 -0700

    JS updates - Randomized (pseudorandom for now) computer choice function complete. - Function to test a single round of the game and compares player choice   to computer choice and returns the result. - Debugging - mostly typos :/

---
+++ javascript.js
@@ -0,0 +1,99 @@
+function getComputerChoice() {
+	let compChoice = (Math.random() * 3);
+	
+	if (computerSelection == 1)
+		return "rock";
+	else if (computerSelection == 2)
+		return "paper";
+	else if (computerSelection == 3)
+		return "scissors";
+}
+function getComputerChoice() {
+        let computerChoice = (Math.round(Math.random() * 3));
+
+        console.log("computerChoice = " + computerChoice);
+
+        if (computerChoice == 0)
+                return "rock";
+        else if (computerChoice == 1)
+                return "paper";
+        else if (computerChoice == 2)
+                return "scissors";
+        else
+          return "The computer blew up!";
+}
+
+function singleRound(playerSelection, computerSelection) {
+
+        computerSelection = getComputerChoice();
+        playerSelection = playerSelection.toLowerCase();
+
+        if (playerSelection == "rock") {
+                if (computerSelection == "rock") {
+                        return "Both players chose rock, it's a draw!";
+                } else if (computerSelection == "paper") {
+                        return "You lose, the computer chose paper!";
+                } else if (computerSelection == "scissors") {
+                        return "You win, the computer chose scissors!";
+                }
+        } else if (playerSelection == "paper") {
+                if (computerSelection == "paper") {
+                        return "Both players chose paper, it's a draw!";
+                } else if (computerSelection == "scissors") {
+                        return "You lose, the computer chose scissors!";
+                } else if (computerSelection == "rock") {
+                        return "You win, the computer chose rock!";
+                }
+        } else if (playerSelection == "scissors") {
+                if (computerSelection == "scissors") {
+                        return "Both players chose scissors, it's a draw!";
+                } else if (computerSelection == "rock") {
+                        return "You lose, the computer chose rock!";
+                } else if (computerSelection == "scissors") {
+                        return "You win, the computer chose paper!";
+                }
+        } else {
+                return "Invalid entry, please choose rock, paper, or scissors.";
+        }
+}
+
+let choice = "rock";
+
+let result = singleRound(choice);
+
+//console.log("result = " + result);
+alert("choice = " + choice + "\nresult = " + result);
+function singleRound(playerSelection, computerSelection) {
+
+	computerSelection == getComputerChoice();
+
+	playerSelection == playerSelection.toLowerCase();
+
+	if (playerSelection == "rock") {
+		if (computerSelection == "rock") {
+			return "Both players chose rock, it's a draw!";
+		} else if (computerSelection == "paper") {
+			return "You lose, the computer chose rock!";
+		} else if (computerSelection == "scissors") {
+			return "You win, the computer chose scissors!";
+		}
+	} else if (playerSelection == "paper") {
+		if (computerSelection == "paper") {
+                        return "Both players chose paper, it's a draw!";
+                } else if (computerSelection == "scissors") {
+                        return "You lose, the computer chose scissors!";
+                } else if (computerSelection == "rock") {
+			return "You win, the computer chose rock!";
+                }
+	} else if (playerSelection == "scissors") {
+		if (computerSelection == "scissors") {
+                        return "Both players chose scissors, it's a draw!";
+                } else if (computerSelection == "rock") {
+                        return "You lose, the computer chose rock!";
+                } else if (computerSelection == "scissors") {
+                        return "You win, the computer chose paper!";
+                }
+	} else {
+		return "Invalid entry, please choose rock, paper, or scissors.";
+	}
+}
