2a7e2f345566f3c5df448a386d0127f643b359e4
Spec-D <dpraegitzer@gmail.com>
2022-07-29 02:38:23 -0700

    JS Updates - Some functionality. Had opponent toggle & 2nd player score name synced at one point.

--- javascript.js
+++ javascript.js
@@ -1,43 +1,8 @@
-function game() {
+var PSCORE = 0;
+var OSCORE = 0;
+var ROUND = 1;
 
-        let score = 0;
-        let compScore = 0;
-
-        for(let i=0; i<4; i++) {
-                playRound();
-                if (playerSelection == "rock" && computerSelection == "rock") {
-                        i--;
-                } else if (playerSelection == "rock" && computerSelection == "paper") {
-                        compScore++;
-                } else if (playerSelection == "rock" && computerSelection == "scissors") {
-                        score++;
-                } else if (playerSelection == "paper" && computerSelection == "rock") {
-                        i--;
-                } else if (playerSelection == "paper" && computerSelection == "paper") {
-                        compScore++;
-                } else if (playerSelection == "paper" && computerSelection == "scissors") {
-                        score++;
-                } else if (playerSelection == "scissors" && computerSelection == "rock") {
-                        i--;
-                } else if (playerSelection == "scissors" && computerSelection == "paper") {
-                        compScore++;
-                } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-                        score++;
-                } else
-                        return "DOES NOT COMPUTE";
-        }
-        if (score > compScore) {
-                return "Score: " + score +
-                       "\ncompScore: " + compSCore +
-                       "\n\nYOU WIN!";
-        } else {
-                return "Score: " + score +
-                       "\ncompScore: " + compSCore +
-                       "\n\nYou lose."
-        }
-}
-
-
+// works
 function getComputerChoice() {
         let computerChoice = (Math.round(Math.random() * 3));
 
@@ -53,44 +18,94 @@ function getComputerChoice() {
           return "DOES NOT COMPUTE";
 }
 
+/* buggy
 function playRound(playerSelection, computerSelection) {
 
+   alert("playRound() call");
         computerSelection = getComputerChoice();
         playerSelection = playerSelection.toLowerCase();
 
-        if (playerSelection == "rock") {
+        if (playerSelection == 'rock') {
+   
                 if (computerSelection == "rock") {
-                        return "Both players chose rock, it's a draw!";
+                        ROUND++;
+                        console.log("Both players chose rock, it's a draw!");
+                
                 } else if (computerSelection == "paper") {
-                        return "You lose, the computer chose paper!";
+                        ROUND++;
+                        OSCORE++;
+                        console.log("You lose this round, the computer chose paper!");
                 } else if (computerSelection == "scissors") {
-                        return "You win, the computer chose scissors!";
+                        ROUND++;
+                        console.log("You win, the computer chose scissors!");
                 }
         } else if (playerSelection == "paper") {
                 if (computerSelection == "paper") {
-                        return "Both players chose paper, it's a draw!";
+                        ROUND++;
+                        console.log("Both players chose paper, it's a draw!");
                 } else if (computerSelection == "scissors") {
-                        return "You lose, the computer chose scissors!";
+                        ROUND++;
+                        OSCORE++;
+                        console.log("You lose, the computer chose scissors!");
                 } else if (computerSelection == "rock") {
-                        return "You win, the computer chose rock!";
+                        ROUND++;
+                        PSCORE++;
+                        console.log("You win, the computer chose rock!");
                 }
         } else if (playerSelection == "scissors") {
                 if (computerSelection == "scissors") {
-                        return "Both players chose scissors, it's a draw!";
+                        ROUND++;
+                        console.log("Both players chose scissors, it's a draw!");
                 } else if (computerSelection == "rock") {
-                        return "You lose, the computer chose rock!";
+                        ROUND++;
+                        OSCORE++;
+                        console.log("You lose, the computer chose rock!");
                 } else if (computerSelection == "scissors") {
-                        return "You win, the computer chose paper!";
+                        ROUND++;
+                        PSCORE++;
+                        console.log("You win, the computer chose paper!");
                 }
         } else {
-                return "Invalid entry, please choose rock, paper, or scissors.";
+                alert("Invalid entry, please choose rock, paper, or scissors.");
         }
+      
+        document.getElementById("playerScore").innerHTML = `Player Score: ${PSCORE}`;
+        document.getElementById("opponentScore").innerHTML = `Opponent Score: ${OSCORE}`;
+  
+        if(ROUND == 5){
+          if(PSCORE > OSCORE){
+              alert("P1 wins");}//Player One Wins ${PSCORE} to ${OSCORE} sent to game log
+          else if(OSCORE > PSCORE){
+              alert("P2/PC wins");}
+        }                       //Player Two / Computer Wins rewrite 
+                                //this to differentiate between computer 
+                                //and player 2 winning
+          
+          PSCORE = 0;
+          OSCORE = 0;
+          ROUND = 1;
+        }  
+*/
+
+// was working, now buggy. see above?
+function opponentButtonToggle() {
+  
+  alert("TEST")
+  PSCORE = 0;
+  OSCORE = 0;
+
+  var oppType = document.getElementById("opponentButton");
+  if(oppType.value == "Human"){
+      oppType.value = "Computer";}
+  else if(oppType.value == "Computer"){
+      oppType.value = "Human";}
+  else{
+      oppType.value = "Computer";}
+ 
+
+  if(oppType.value == "Human") {
+    document.getElementById("opponentScore").innerHTML = `Player 2 Score: ${OSCORE}`;}
+  else if(oppType.value == "Computer"){
+    document.getElementById("opponentScore").innerHTML = `Computer Score: ${OSCORE}`;}
+
 }
-
-choice = prompt(choice);
-
-let result = playRound(choice);
-
-//console.log("result = " + result);
-alert(game());
-
